7671091ed1bdf8cebd5c788476b512a8664116fb
Manan10Sharma <77826107+Manan10Sharma@users.noreply.github.com>
2021-02-06 06:28:18 -1000

    Create detail.py

---
+++ detail.py
@@ -0,0 +1,8 @@
+email=input("Enter your Email:")
+nm=input("Enter your Name:")
+mob=int(input("Enter your Mobile_no.:"))
+roll=int(input("Enter your Roll_no.:"))
+print("\n Email:",email)
+print("\n Name:",nm)
+print("\n Mobile no.:",mob)
+print("\n Roll no.:",roll)
