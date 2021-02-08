e4b66051eec388bd7c3d9865262169a0466104e0
Manan10Sharma <77826107+Manan10Sharma@users.noreply.github.com>
2021-02-08 00:41:28 -1000

    Create ass_dss.py

---
+++ ass_dss.py
@@ -0,0 +1,25 @@
+arr=[2,4,6,8,9];
+temp = 0;
+print("Elements of array: ");
+for x in range(0, len(arr)):
+    print(arr[x], end=" ");
+for x in range(0, len(arr)):
+    for y in range(x+1, len(arr)):
+        if(arr[x] > arr[y]):
+            temp = arr[x]
+            arr[x] = arr[y]
+            arr[y] = temp
+print();
+print("Elements of array in ascending order: ")
+for x in range(0, len(arr)):
+    print(arr[x], end=" ")
+
+for x in range(0, len(arr)):
+    for y in range(x+1, len(arr)):
+        if(arr[x] < arr[y]):
+            temp = arr[x];
+            arr[x] = arr[y];
+            arr[y] = temp;
+print("\nElements of array in descending order: ");
+for x in range(0, len(arr)):
+    print(arr[x],end=" ")
