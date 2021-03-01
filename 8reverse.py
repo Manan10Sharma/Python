e261b0d16964cfb2db328ef3b68cf4bb7c79cb54
Manan Sharma <77826107+Manan10Sharma@users.noreply.github.com>
2021-02-28 18:21:58 -1000

    Update and rename reverse.py to 8reverse.py

--- 8reverse.py
+++ 8reverse.py
@@ -1,5 +1,4 @@
-print("\t\t #Reverse of string...")
+print("\t\t Reverse of string...")
 name=input("Enter your name :-")
 reverse=name[-1::-1]
 print(f"Reverse of your name is :-{reverse}")
-print("\t\t #Program Finished...")
