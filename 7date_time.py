7e9460fd93d64b006fc5ff5164ed245cf16d2e4e
Manan Sharma <77826107+Manan10Sharma@users.noreply.github.com>
2021-02-28 18:21:03 -1000

    Update and rename date_time.py to 7date_time.py

--- 7date_time.py
+++ 7date_time.py
@@ -1,7 +1,6 @@
 from datetime import datetime
 now=datetime.now()
-print("\t\t#current date and time ")
+print("\t\tcurrent date and time ")
 print("now:-",now)
 dt_string=now.strftime("%D--%M--%Y  %H:%M:%S")
 print("date and time:-",dt_string)
-print("\t\t\tprogram finished...")
