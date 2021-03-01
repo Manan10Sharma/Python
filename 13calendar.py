3d3ac7c1a1346d431fd485a28e34525b02fe4898
Manan Sharma <77826107+Manan10Sharma@users.noreply.github.com>
2021-02-28 23:00:39 -1000

    Update and rename calendar.py to 13calendar.py

--- 13calendar.py
+++ 13calendar.py
@@ -1,6 +1,6 @@
-print("\t\t #Calendar...")
+#printing calender
+print("\t\t Calendar...")
 import calendar
 y=int(input("input year:"))
 m=int(input("input month:"))
 print((calendar.month(y,m)))
-print("\t\t #Program Finished...")
